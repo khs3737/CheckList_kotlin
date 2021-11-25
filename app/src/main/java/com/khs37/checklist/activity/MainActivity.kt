691f91f2fbe24b2074c52bd7702416897dfb75bc
Kim HyungSeok <zx7589@gmail.com>
2021-11-25 23:50:00 +0900

    프로젝트 세팅

---
+++ app/src/main/java/com/khs37/checklist/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.khs37.checklist.activity
+
+import android.app.Activity
+import android.os.Bundle
+import com.khs37.checklist.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
