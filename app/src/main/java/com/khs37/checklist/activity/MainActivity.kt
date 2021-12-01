16a9dc181eb109eaed180ef24e2cea0e7db945a0
Kim HyungSeok <zx7589@gmail.com>
2021-12-01 15:44:47 +0900

    21.12.01

--- app/src/main/java/com/khs37/checklist/activity/MainActivity.kt
+++ app/src/main/java/com/khs37/checklist/activity/MainActivity.kt
@@ -3,12 +3,25 @@ package com.khs37.checklist.activity
 import android.app.Activity
 import android.os.Bundle
 import com.khs37.checklist.R
-import kotlinx.android.synthetic.main.activity_main.*
+import com.khs37.checklist.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
+
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.textView.setText("안녕하세요")
+
+    }
+
+    override fun onDestroy() {
+        mBinding = null
+        super.onDestroy()
     }
 }
